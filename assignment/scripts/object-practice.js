b96c2c4af61804fc8c6a756e1f86e11cb3a20312
Ashley Wessel <amerino.dance@gmail.com>
2023-09-05 12:23:22 -0500

    coded some

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -10,8 +10,16 @@ console.log('***** Object Practice *****')
 */
 const me = {
   // TODO - add properties here
+  firstName: 'Ashley',
+  lastName: 'Wessel',
+  hasSiblings: true,
+  shoeCount: 35,
+  favThreeFoods: ['pizza', 'pasta', 'sandwiches'],
 };
 console.log('A little about me:', me);
+
+
+
 
 /* 2. Accessing object properties.
   - Create a variable called fullName 
@@ -20,6 +28,8 @@ console.log('A little about me:', me);
   - Console.log fullName
 */
 
+let fullName = me.firstName + ' ' + me.lastName;
+console.log(fullName);
 
 
 /* 3. Nested arrays
@@ -27,6 +37,8 @@ console.log('A little about me:', me);
   - Console.log your last favorite food 
 */
 
+console.log(me.favThreeFoods[0]);
+console.log(me.favThreeFoods[me.favThreeFoods.length-1]);
 
 
 /* 4. Change a property of an existing object.
@@ -36,6 +48,11 @@ console.log('A little about me:', me);
   - Console.log your updated shoe count. 
 */
 
+console.log(me.shoeCount);
+me.shoeCount += 1;
+console.log(me.shoeCount);
+
+
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
@@ -43,3 +60,26 @@ console.log('A little about me:', me);
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+
+me.favoriteColor = 'blue';
+console.log(me.favoriteColor);
+
+
+
+
+//appending to the DOM
+
+function displayMe(meInput) {
+  let meList = document.querySelector('#me');
+  meList.innerHTML = '';
+  //for(let fact of meInput) {
+    //console.log(fact);
+    meList.innerHTML += `
+    <h2>Full Name: ${fullName}</h2>
+    <p>Do I have siblings? ${me.hasSiblings}</p>
+    <p>How many pairs of shoes do I own? ${me.shoeCount}</p>
+    <p>What are my three favorite foods? ${me.favThreeFoods}</p>
+    `;
+  //}
+}
+displayMe(me);
